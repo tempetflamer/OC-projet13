0109b3df1ee8ac2513395c7a55755249285dbc4a
tempet <tempetflamer@gmail.com>
2023-05-19 15:36:10 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Error404/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Error404 () {
+  return (
+    <div>Text</div>
+  )
+}
+
+Error404.propTypes = {
+
+}
